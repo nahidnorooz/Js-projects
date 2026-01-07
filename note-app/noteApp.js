377c58129b2dfec11d82895799e39b257e055e50
nahidnorooz <nahidnorooz@gmail.com>
2026-01-07 22:17:16 +0330

    Notes get added to note container

--- note-app/noteApp.js
+++ note-app/noteApp.js
@@ -3,6 +3,7 @@
 const modalScreen = document.querySelector(".modal-screen");
 const modal = document.querySelector(".modal");
 const xButton = document.querySelector(".x-btn");
+
 const notesText = document.querySelector(".text");
 
 const createNote = document.querySelector(".create-note");
@@ -10,12 +11,14 @@ const createNote = document.querySelector(".create-note");
 const createBtn = document.querySelector(".create");
 const cancleBtn = document.querySelector(".cancle");
 
+const notesContainer = document.querySelector(".notes-container");
+
 
 function showModal() {
     modalScreen.classList.remove("hidden");
 }
 function hideModal() {
-    modalScreen.classst.add("hiddLien");
+    modalScreen.classList.add("hidden");
 }
 
 
@@ -23,11 +26,31 @@ createNote.addEventListener("click", showModal);
 
 xButton.addEventListener("click", hideModal);
 
-createBtn.addEventListener("click", hideModal);
-cancleBtn.addEventListener("click", hideModal);
+let newNote;
+
+createBtn.addEventListener("click", function () { 
+    hideModal();
+    let userNote = notesText.value;
+
+    let newNote = document.createElement("div");
+    newNote.className = "note";
+    newNote.innerHTML = userNote;
+
+    notesContainer.append(newNote);
+
+    notesText.value = "";
+    
+   
+});
+
+cancleBtn.addEventListener("click", function () {
+    hideModal();
+    notesText.value = "";
+}); 
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
         hideModal();
     }
 });
+
