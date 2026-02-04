acd3b4335c058f0870cf901cacffab76c1eb1d41
nahidnorooz <nahidnorooz@gmail.com>
2026-02-04 13:10:53 +0330

    Added project

---
+++ music-player/README.txt
@@ -0,0 +1,19 @@
+# Muisc Player
+
+A web-based music player built with JavaScript that provides interactive audio playback and media controls directly in the browser.
+
+## Features
+- Play and pause audio tracks
+- Skip forward and backward by 15 seconds
+- next and previous track navigation
+- Dynamic track handling
+- clean and responsive interface
+
+## Tech Used
+- HTML
+- CSS
+- JavaScript
+
+## How to run
+1. Open `index.html` file in a browser.
+2. Use the player controls to navigate through tracks and control playback.
---
+++ music-player/source/app.js
@@ -0,0 +1,81 @@
+
+const music = document.querySelector("audio");
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnImg = document.querySelector(".play-img");
+const skipBackBtn = document.querySelector(".skip-back");
+const skipAheadBtn = document.querySelector(".skip-ahead");
+const previousBtn = document.querySelector(".previous-btn");
+const nextBtn = document.querySelector(".next-btn");
+const musicCover = document.querySelector(".cover-img");
+const musicTitle = document.querySelector(".music-title");
+
+const musics = [
+{id: 1, src: "/music-player/audios/Little Nightmares II (Main Theme)   Tobias Lilja.m4a", cover: "/music-player/imgaes/little nightmares.jpg", title: "Little Nightmares II (Main Theme)"},
+{id: 2, src: "/music-player/audios/Samorost-3-Soundtrack-08-Prenatal-Hunters-Floex-Revision-Flo.m4a", cover: "/music-player/imgaes/samorost3.jpeg", title: "Samorost 3 soundtrack"},
+{id: 3, src: "/music-player/audios/rises the moon   liana flores.mp3", cover: "/music-player/imgaes/rises-the-moon.jpeg", title: "Rises the moon"}
+]
+
+let mainMusicIndex = 0;
+
+function toggle() {
+    if (toggleBtn.className.includes("pause")) {
+        music.play();
+        toggleBtnImg.setAttribute("src", "/music-player/imgaes/pause.png");
+        toggleBtn.classList.remove("pause");
+    } else {
+        music.pause();
+        toggleBtnImg.setAttribute("src", "/music-player/imgaes/play.png");
+        toggleBtn.classList.add("pause");
+    }
+}
+
+function skipBack() {
+  music.currentTime -= 15;
+}
+function skipAhead() {
+    music.currentTime += 15;
+
+}
+
+function nextSong() {
+   mainMusicIndex ++;
+
+   if (mainMusicIndex > 2) {
+    mainMusicIndex = 0;
+   }
+   const mainMusic = musics[mainMusicIndex];
+   musicCover.setAttribute("src", mainMusic.cover);
+   music.setAttribute("src", mainMusic.src);
+   musicTitle.innerHTML = mainMusic.title;
+   
+   music.play();
+   toggleBtnImg.setAttribute("src", "/music-player/imgaes/pause.png");
+   toggleBtn.classList.remove("pause");
+   
+}
+function previousSong() {
+    mainMusicIndex --;
+    
+    if (mainMusicIndex < 0) {
+        mainMusicIndex = 2;  
+    }
+    const mainMusic = musics[mainMusicIndex];
+    musicCover.setAttribute("src", mainMusic.cover);
+    music.setAttribute("src", mainMusic.src);
+    musicTitle.innerHTML = mainMusic.title;
+
+    music.play();
+    toggleBtnImg.setAttribute("src", "/music-player/imgaes/pause.png");
+    toggleBtn.classList.remove("pause");
+}
+
+toggleBtn.addEventListener("click", toggle);
+skipBackBtn.addEventListener("click", skipBack);
+skipAheadBtn.addEventListener("click", skipAhead);
+previousBtn.addEventListener("click", previousSong);
+nextBtn.addEventListener("click", nextSong);
+
+
+
+
