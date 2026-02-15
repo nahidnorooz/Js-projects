7b65d999279b2e02c9c8fd4d54eb4e672f36cfe0
nahidnorooz <nahidnorooz@gmail.com>
2026-02-15 15:03:50 +0330

    perf: fixed music card button's double click bug

--- music-player/source/app.js
+++ music-player/source/app.js
@@ -7,29 +7,32 @@ const nextBtn = document.querySelector(".next-btn");
 const toggleBtnImg = document.querySelector(".play-img");
 
 function toggle() {
-    if (toggleBtn.className.includes("paused")) {
+        if (toggleBtn.className.includes("paused")) {
         toggleBtnImg.setAttribute("src", "/music-player/image/paused.png");
         toggleBtn.classList.remove("paused");
+        music.pause();
     } else {
         toggleBtnImg.setAttribute("src", "/music-player/image/playing.png");
         toggleBtn.classList.add("paused");
+        music.play();
     }
 }
 
-toggleBtn.addEventListener("click", toggle);
 playBtns.forEach(function (playBtn) {
 
-        playBtn.addEventListener("click", function(event) {            
+        playBtn.addEventListener("click", function() {            
             const mainSrc = playBtn.dataset.src;
             music.setAttribute("src", mainSrc);
             if (playBtn.className.includes("paused")) {
-                playBtn.lastChild.setAttribute("src", "/music-player/image/paused.png");
-                playBtn.classList.remove("paused");
-                music.pause();
-            } else {
                 playBtn.lastChild.setAttribute("src", "/music-player/image/playing.png");
-                playBtn.classList.add("paused");
+                playBtn.classList.remove("paused");
                 music.play();
+            } else {
+                playBtn.lastChild.setAttribute("src", "/music-player/image/paused.png");
+                playBtn.classList.add("paused");
+                music.pause();
             }
         });
     });
+
+toggleBtn.addEventListener("click", toggle);
